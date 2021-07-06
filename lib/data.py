1c86ca6769558d97bd450dc8113d04f80916e0a5
aksh-ai <akshayakki6899@gmail.com>
2021-07-06 20:11:31 +0530

    Updated data

--- lib/data.py
+++ lib/data.py
@@ -1,7 +1,7 @@
 import torch as th
 import torchio as tio
 import multiprocessing
-from torch.utils.data import Dataset, DataLoader, random_split
+from sklearn.model_selection import train_test_split
 from lib.utils import get_train_transforms, get_validation_transforms
 
 def get_dataset_from_path(image_paths, label_paths):
@@ -18,40 +18,49 @@ def get_dataset_from_path(image_paths, label_paths):
 
     return tio.SubjectsDataset(subjects)
 
-def get_datasets(images, 
-                labels, 
-                test_size=0.1, 
-                random_state=51, 
-                volume="whole", 
-                patch_size=128,
-                samples_per_volume=128,
-                max_queue_length=128,
-                num_workers=multiprocessing.cpu_count()):
-    dataset = get_dataset_from_path(images, labels)
-    
-    data_length = len(dataset)
+def load_datasets(
+    image_paths, 
+    label_paths, 
+    test_size=0.1, 
+    random_state=None, 
+    volume="whole", 
+    patch_size=128,
+    samples_per_volume=128,
+    max_queue_length=128):
+    assert len(image_paths) == len(label_paths), "Number of samples and labels are not equal"
+
+    data_length = len(image_paths)
     validation_length = int(test_size * data_length)
     training_length = data_length - validation_length
 
-    training_set, validation_set= random_split(dataset, (training_length, validation_length), generator=th.Generator().manual_seed(random_state))
+    X_train, X_valid, y_train, y_valid = train_test_split(image_paths, label_paths, test_size=test_size, random_state=random_state)
+
+    training_set = []
+    
+    for image_path, label_path in zip(X_train, y_train):
+        subject = tio.Subject(
+            mri=tio.ScalarImage(image_path),
+            brain=tio.LabelMap(label_path),
+        )
+        training_set.append(subject) 
 
     training_set = tio.SubjectsDataset(training_set, transform=get_train_transforms())
 
+    validation_set = []
+    
+    for image_path, label_path in zip(X_valid, y_valid):
+        subject = tio.Subject(
+            mri=tio.ScalarImage(image_path),
+            brain=tio.LabelMap(label_path),
+        )
+        validation_set.append(subject)
+
     validation_set = tio.SubjectsDataset(validation_set, transform=get_validation_transforms())
 
-    print(f"Dataset: {len(dataset)} Images")
-    print(f'Training set: {len(training_set)} Images')
-    print(f'Validation set: {len(validation_set)} Images')
-
-    if volume.lower() == 'whole':
-        print("Whole images generated")
-        return training_set, validation_set
-    
-    elif volume.lower() in ['patch', 'patches']:
-        print("Patches generated")
+    if volume.lower() in ['patch', 'patches']:
         sampler = tio.data.UniformSampler(patch_size)
         
-        training_patches = tio.Queue(
+        training_set = tio.Queue(
             subjects_dataset=training_set,
             max_length=max_queue_length,
             samples_per_volume=samples_per_volume,
@@ -60,7 +69,7 @@ def get_datasets(images,
             shuffle_patches=True,
         )
 
-        validation_patches = tio.Queue(
+        validation_set = tio.Queue(
             subjects_dataset=validation_set,
             max_length=max_queue_length,
             samples_per_volume=samples_per_volume,
@@ -68,5 +77,9 @@ def get_datasets(images,
             shuffle_subjects=False,
             shuffle_patches=False,
         )
-
-        return training_patches, validation_patches
+    
+    print(f"Volume Mode: {volume.upper()} | Dataset: {len(image_paths)} Images")
+    print(f'Training set: {len(training_set)} Images')
+    print(f'Validation set: {len(validation_set)} Images')
+    
+    return training_set, validation_set
