1839e6d8cf602559c3162166327293f140f40b22
aksh-ai <akshayakki6899@gmail.com>
2021-07-11 23:55:50 +0530

    added ica script

---
+++ ica.py
@@ -0,0 +1,13 @@
+# Author: Akshay Kumaar M (aksh-ai)
+
+# Necessary imports
+# add ica inference script
+# add readme with fabulous explanations
+    # about project
+    # env setup
+    # scripts running and commands
+    # lib and modules
+    # notebooks
+    # results
+    # stats
+# fix req.txt and conda.yml
